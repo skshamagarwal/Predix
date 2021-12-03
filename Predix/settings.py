ac3d5e470ff0846389720466481ede853a47739a
skshamagarwal <skshamagarwal@gmail.com>
2021-12-03 21:33:29 +0530

    Updated Static Root

--- Predix/settings.py
+++ Predix/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join('static'), )
 
